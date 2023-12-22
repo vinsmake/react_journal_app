5146daf46b9626307e670bf1bd2805bbbf1bf829
vinsmake <ps.enrique.e@gmail.com>
2023-12-22 13:38:40 -0600

    Project updated

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,12 +1,17 @@
 
 import { Typography } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
+import { NothingSelectedView } from "../view/NothingSelectedView"
+import { NoteView } from "../view/NoteView"
 
 export const JournalPage = () => {
     return (
     <JournalLayout>
-        <Typography variant="h1">JournalPage </Typography>
+{/*         <Typography variant="h1">JournalPage </Typography>
         <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit quaerat expedita libero temporibus molestias doloribus, repudiandae laudantium iure inventore ratione? Labore dicta voluptas illum modi molestiae, molestias quia soluta odit.</Typography>
+ */}
+    {/* <NothingSelectedView/> */}
+    <NoteView/>
     </JournalLayout>
     )
 }
