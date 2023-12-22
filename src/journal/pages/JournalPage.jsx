7eb3d385e45ebe7f9e4a63a401bde5007de941d9
vinsmake <ps.enrique.e@gmail.com>
2023-12-22 13:40:34 -0600

    Project updated

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,8 +1,9 @@
 
-import { Typography } from "@mui/material"
+import { IconButton, Typography } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
 import { NothingSelectedView } from "../view/NothingSelectedView"
 import { NoteView } from "../view/NoteView"
+import { AddOutlined } from "@mui/icons-material"
 
 export const JournalPage = () => {
     return (
@@ -10,8 +11,22 @@ export const JournalPage = () => {
 {/*         <Typography variant="h1">JournalPage </Typography>
         <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit quaerat expedita libero temporibus molestias doloribus, repudiandae laudantium iure inventore ratione? Labore dicta voluptas illum modi molestiae, molestias quia soluta odit.</Typography>
  */}
-    {/* <NothingSelectedView/> */}
-    <NoteView/>
+    <NothingSelectedView/>
+    {/* <NoteView/> */}
+
+    <IconButton
+        size='large'
+        sx={{
+          color: 'white',
+          backgroundColor: 'error.main',
+          ':hover': { backgroundColor: 'error.main', opacity: 0.9 },
+          position: 'fixed',
+          right: 50,
+          bottom: 50
+        }}
+      >
+        <AddOutlined sx={{ fontSize: 30 }} />
+      </IconButton>
     </JournalLayout>
     )
 }
